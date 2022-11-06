9a17b4f7315c8910eb64611c260849b6756aac4e
song <55rsearchco@gmail.com>
2022-11-06 21:58:26 +0900

    add responsive

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,27 +1,29 @@
 class Youtube {
-  constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: 'GET'
-    };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   async mostPopular() {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/videos?key=${this.key}&part=snippet&chart=mostPopular&maxResults=25`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items;
+    const response = await this.youtube.get('videos', {
+      params: {
+        part: 'snippet',
+        chart: 'mostPopular',
+        maxResults: 25,
+      }
+    });
+    return response.data.items;
   }
 
   async search(query) {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?key=${this.key}&part=snippet&maxResults=25&q=${query}&type=video`,
-      this.getRequestOptions
-    );
-    const result = await response.json();
-    return result.items.map(item => ({...item, id: item.id.videoId }));
+    const response = await this.youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 25,
+        type: 'video',
+        q: query,
+      }
+    });
+    return response.data.items.map(item => ({ ...item, id: item.id.videoId }));
   }
 }
 
